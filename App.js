3f89bac55bbf423f3ab046b89dd29fbe8505865c
Garvit Bansal <garvit.bansal29@gmail.com>
2021-02-06 14:27:00 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+// import 'react-native-gesture-handler';
+import React from 'react';
+import {Text, View} from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test heheheh</Text>
+    </View>
+  );
+};
+
+export default App;
